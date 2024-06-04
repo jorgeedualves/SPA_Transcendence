95b741e4f06d8186967657d4d6c79ee1cb210225
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-04 20:58:45 -0300

    add game to project

--- game/views.py
+++ game/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+@ensure_csrf_cookie
+def singleGame(request):
+    is_authenticated = request.user.is_authenticated 
+    body = json.loads(request.body.decode('utf-8'))  # Decodifica e carrega o JSON diretamente
+
+    context = {
+        'playerOneName': body.get('playerOneName'),
+        'playerTwoName': body.get('playerTwoName'),
+        'skin': body.get('skin'),
+        'is_authenticated': is_authenticated
+    }
+
+    return JsonResponse(context)
