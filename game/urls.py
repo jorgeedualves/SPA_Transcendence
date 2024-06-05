3d83569ef3e9bc48c4c0c6538a96a32103463c17
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-05 16:47:26 -0300

    ajust game logic

--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,6 @@ from core import views  # Importa o módulo views do core
 # Lista de padrões de URL para o aplicativo
 urlpatterns = [
     path('options/', views.options, name='options'),
-	path('singleGame/', views.singleGame, name='singleGame'),
 	path('pong/', views.pong, name='pong'),
+	path('tournament/', views.tournament, name='tournament'),
 ]
