29f5f9e176a55f34236b7b66996ac2133f641271
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-06 23:53:57 -0300

    complete login feature

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path  # Importa a função path do módulo django.urls
-from core import views  # Importa o módulo views do core
+from . import views  # Importa o módulo views do core
 
 # Lista de padrões de URL para o aplicativo
 urlpatterns = [
