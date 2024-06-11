7a529c3ebe64da8112bb02a16d00913a7d7cab09
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-10 23:18:21 -0300

    change game views to accept translation

--- game/views.py
+++ game/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
+from django.utils import translation
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 
 @ensure_csrf_cookie
 def options(request):
+    language = request.headers.get('Content-Language', 'en')
+    translation.activate(language)
     if request.headers.get('X-Requested-With') == 'Fetch':
         return render(request, 'options.html')
     return render(request, 'index.html')
 
 @ensure_csrf_cookie
 def pong(request):
+    language = request.headers.get('Content-Language', 'en')
+    translation.activate(language)
     if request.headers.get('X-Requested-With') == 'Fetch':
         is_authenticated = request.user.is_authenticated
         body = json.loads(request.body.decode('utf-8'))
@@ -24,6 +29,8 @@ def pong(request):
 
 @ensure_csrf_cookie
 def tournament(request):
-	if request.headers.get('X-Requested-With') == 'Fetch':
-		return render(request, 'tournament.html')
-	return render(request, 'index.html')
+    language = request.headers.get('Content-Language', 'en')
+    translation.activate(language)
+    if request.headers.get('X-Requested-With') == 'Fetch':
+        return render(request, 'tournament.html')
+    return render(request, 'index.html')
