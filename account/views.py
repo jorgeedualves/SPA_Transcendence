17882dd30d343c52a6a080c817b03d37d91a1bc3
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-10 23:13:15 -0300

    change account template

--- account/views.py
+++ account/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils import translation
 
 @ensure_csrf_cookie
 def account(request):
+    language = request.headers.get('Content-Language', 'en')
+    translation.activate(language)
     if request.headers.get('X-Requested-With') == 'Fetch':
         return render(request, 'account.html')
     return render(request, 'index.html')
