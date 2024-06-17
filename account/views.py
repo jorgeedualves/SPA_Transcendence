1e56c63517d35e3e086d0c11c542956092e78386
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-17 00:17:43 -0300

    change account template to hold user picture

--- account/views.py
+++ account/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils import translation
 
+from django.shortcuts import render
+from django.utils import translation
+
 @ensure_csrf_cookie
 def account(request):
     language = request.headers.get('Content-Language', 'en')
     translation.activate(language)
+    
+    user = request.user
+    profile_picture_url = user.profile_picture_url if user.profile_picture_url else 'static/images/default_picture.png'
+    context = {
+        'profile_picture_url': profile_picture_url,
+    }
     if request.headers.get('X-Requested-With') == 'Fetch':
-        return render(request, 'account.html')
-    return render(request, 'index.html')
+        return render(request, 'account.html', context)
+    return render(request, 'index.html', context)
