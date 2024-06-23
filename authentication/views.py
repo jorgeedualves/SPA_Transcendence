d961db495e0f2bf162914e42ea1e87ffa2034b2d
hmigl <88746406+hmigl@users.noreply.github.com>
2024-06-23 18:05:51 -0300

    hell yeah

--- authentication/views.py
+++ authentication/views.py
@@ -1,34 +1,36 @@
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from urllib.parse import urlencode
-from django.contrib.auth import login, authenticate #, logout
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.contrib.auth.decorators import login_required
-from .services import exchange_code, generate_jwt_token
-from .auth import IntraAuthenticationBackend
 import os
+from urllib.parse import urlencode, urlparse, parse_qs
+
+from django.contrib.auth import login, authenticate  # , logout
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.utils import translation
-from game.views import options
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django_otp.plugins.otp_totp.models import TOTPDevice
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from game.views import options
 from .auth import IntraAuthenticationBackend
 from .forms import CreateUserForm, LoginForm
 from .services import exchange_code, generate_jwt_token
+
 
 @ensure_csrf_cookie
 def index(request):
     return render(request, 'index.html')
 
+
 @ensure_csrf_cookie
 def initial_content(request):
     if request.headers.get('X-Requested-With') == 'Fetch':
         is_authenticated = request.user.is_authenticated
-        
+
         if is_authenticated:
             return options(request)
         else:
             return login_view(request)
     return render(request, 'index.html')
+
 
 def login_view(request):
     language = request.headers.get('Content-Language', 'en')
@@ -39,9 +41,11 @@ def login_view(request):
     auth_url = os.environ.get("AUTH_URL_INTRA")
     return render(request, 'login.html', {'auth_url': auth_url})
 
+
 def intra_login(request):
     auth_url = os.environ.get("AUTH_URL_INTRA")
     return redirect(auth_url)
+
 
 def intra_login_redirect(request):
     code = request.GET.get("code")
@@ -65,6 +69,7 @@ def intra_login_redirect(request):
     except Exception as e:
         print(f"Error during authentication: {e}")
         return JsonResponse({"error": "Authentication failed"}, status=401)
+
 
 # def logout_view(request):
 #     logout(request)
@@ -92,7 +97,15 @@ def register(request):
             user = form.save()
             device = TOTPDevice.objects.create(user=user, name='default')
             totp_url = device.config_url + '&' + urlencode({'issuer': 'ft_transcendence', 'label': user.email})
-            context = {'totp_url': totp_url}
+            parsed_url = urlparse(totp_url)
+            query_params = parse_qs(parsed_url.query)
+            context = {
+                'secret': query_params.get('secret', [''])[0],
+                'algorithm': query_params.get('algorithm', [''])[0],
+                'issuer': query_params.get('issuer', [''])[0],
+                'user': parsed_url.path.split('/')[-1],
+                'totp_url': totp_url,
+            }
             return render(request, 'otp_setup.html', context)
     return render(request, 'index.html')
 
