9b0399a3af78c6ddde1cfe7a603873ba1be8c698
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-06-25 14:10:22 -0300

    handle bad request empty form

--- game/views.py
+++ game/views.py
@@ -15,34 +15,50 @@ def options(request):
         return render(request, 'options.html', context)
     return render(request, 'index.html')
 
-@ensure_csrf_cookie
 def pong(request):
     language = request.headers.get('Content-Language', 'en')
     translation.activate(language)
+    
     if request.headers.get('X-Requested-With') == 'Fetch':
-        is_authenticated = request.user.is_authenticated
-        body = json.loads(request.body.decode('utf-8'))
+        try:
+            body = request.body.decode('utf-8')
+            if not body:
+                return JsonResponse({"error": "Empty request body"}, status=400)
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+        
         context = {
-            'playerOneName': body.get('playerOneName'),
-            'playerTwoName': body.get('playerTwoName'),
-            'skin': body.get('skin'),
-            'is_authenticated': is_authenticated
+            'playerOneName': data.get('playerOneName'),
+            'playerTwoName': data.get('playerTwoName'),
+            'skin': data.get('skin'),
+            'is_authenticated': request.user.is_authenticated
         }
         return render(request, 'pong.html', context)
+    
     return render(request, 'index.html')
 
 @login_required
 def tournament(request):
     language = request.headers.get('Content-Language', 'en')
     translation.activate(language)
+    
     if request.headers.get('X-Requested-With') == 'Fetch':
-        body = json.loads(request.body.decode('utf-8'))
+        try:
+            body = request.body.decode('utf-8')
+            if not body:
+                return JsonResponse({"error": "Empty request body"}, status=400)
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+
         context = {
-            'playerOneName': body.get('playerOneName'),
-            'playerTwoName': body.get('playerTwoName'),
-            'skin': body.get('skin'),
+            'playerOneName': data.get('playerOneName'),
+            'playerTwoName': data.get('playerTwoName'),
+            'skin': data.get('skin'),
         }
         return render(request, 'tournament.html', context)
+    
     return render(request, 'index.html')
 
 @login_required
